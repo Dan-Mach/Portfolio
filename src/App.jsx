d26b8bda3a068de165726321b93817493b872fbc
Dan-Mach <danreech83@gmail.com>
2026-05-31 16:19:00 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero';
 import Skills from './components/Skills';
 import SelectedWork from './components/SelectedWork';
 import Experience from './components/Experience';
-import Education from './components/Education';
 import Contact from './components/Contact';
 
 export default function App() {
@@ -15,7 +14,6 @@ export default function App() {
         <Skills />
         <SelectedWork />
         <Experience />
-        <Education />
         <Contact />
       </main>
     </div>
