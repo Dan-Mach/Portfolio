76cd16168b162c06b1f76df66cd17314a299f83d
Dan-Mach <danreech83@gmail.com>
2026-06-02 18:27:34 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ export default function App() {
         <Hero />
         <SelectedWork />
         <Experience />
-        <Contact />
         <Skills />
+        <Contact />
       </main>
     </div>
   );
